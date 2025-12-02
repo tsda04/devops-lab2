0e27aa07ab91b598e772c7255a62221264657526
Цыганова Дарья <135041193+tsda04@users.noreply.github.com>
2025-12-02 06:10:44 +0400

    Create init_db.py

---
+++ app/init_db.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+db = "/data/data.db"
+
+conn = sqlite3.connect(db)
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, message TEXT)")
+cur.execute("INSERT INTO test(message) VALUES ('Hello from SQLite DB!')")
+conn.commit()
+conn.close()
+
+print("DB initialized.")
