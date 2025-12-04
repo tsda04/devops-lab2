8935b7c88f39891f475df84bab58e0110f823aa4
Цыганова Дарья <135041193+tsda04@users.noreply.github.com>
2025-12-04 19:47:06 +0400

    Update init_db.py

--- app/init_db.py
+++ app/init_db.py
@@ -5,8 +5,11 @@ db = "/data/data.db"
 conn = sqlite3.connect(db)
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, message TEXT)")
-cur.execute("INSERT INTO test(message) VALUES ('Hello from SQLite DB111!')")
+# Пересоздаём таблицу при каждом деплое
+cur.execute("DROP TABLE IF EXISTS test")
+cur.execute("CREATE TABLE test (id INTEGER PRIMARY KEY, message TEXT)")
+cur.execute("INSERT INTO test(message) VALUES ('Hello from SQLite DB UPDATED!')")
+
 conn.commit()
 conn.close()
 
